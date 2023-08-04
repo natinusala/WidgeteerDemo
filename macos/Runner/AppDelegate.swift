79e6c9c9129926b72cd05b6650971c894faf67b9
natinusala <natinusala@gmail.com>
2023-08-04 20:29:30 +0200

    macOS support

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -1,6 +1,8 @@
 import Cocoa
 import FlutterMacOS
 
+@_exported import WidgeteerDemo
+
 @NSApplicationMain
 class AppDelegate: FlutterAppDelegate {
   override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
